4cf0197fb0a6ad1a478dabb062738b7adf13a062
ChemaKing13 <jm2000ho@gmail.com>
2026-03-04 21:52:50 -0600

    read operations set

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from fastapi import FastAPI
+from datetime import datetime
+from fastapi import HTTPException
 
 
-app = FastAPI()
+app = FastAPI(root_path="/api/v1")
+
+data = [
+    {
+        "campaign_id": 1, 
+        "name": "Summer Launch", 
+        "due_date": datetime.now(), 
+        "created_at": datetime.now()
+    }, 
+    {
+        "campaign_id": 2, 
+        "name": "Black Friday", 
+        "due_date": datetime.now(), 
+        "created_at": datetime.now()
+    }, 
+    {
+        "campaign_id": 3, 
+        "name": "Pao House", 
+        "due_date": datetime.now(), 
+        "created_at": datetime.now()
+    }
+]
+
+
 
 
 @app.get("/")
 async def root(): 
     return {"message": "Hello workd!"}
+
+@app.get("/campaigns")
+async def read_campaigns(): 
+    return {"campaigns": data}
+
+@app.get("/campaigns/{id}")
+async def read_campaign(id: int):
+    for campaign in data: 
+        if campaign.get("campaign_id") == id: 
+            return {"campaign": campaign}
+        raise HTTPException(status_code=404)
